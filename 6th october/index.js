9aa73eb1676b2e1daee2a53640245e0808437b03
kinguzoma3 <142968007+kinguzoma3@users.noreply.github.com>
2023-10-09 06:24:55 +0100

    Add files via upload

---
+++ 6th october/index.js
@@ -0,0 +1,99 @@
+
+
+//if/else
+
+let score = 80
+if (score<49) { console.log("failed")
+
+     if (score>49<59) {console.log("good")
+        
+    }
+    else if (score>59<69) {console.log("very good")
+        
+    }
+    else if (score>69) {console.log("excellent")
+        
+    }
+
+    //arrays
+
+    const footballClubs = ["man utd","chelshit","bayern", "assnal","liverpool","wolves", "madrid","barca" "granad", "osasuana","mancity","westham","intermilan","luton","avfc",]
+
+    console.log("footballclubs")
+
+    // objects
+const studentOne={
+firstName:"king",
+lastName: "cash",
+phoneNo: "08187369096",
+address: "choba",
+age:15,
+gender: "male",
+email: "kingMail.com",
+score: 230
+}
+console.log(studentOne)
+
+const studentTwo={
+firstName:"fortune",
+lastName: "gilbert",
+phoneNo: "0812665431",
+address: "choba",
+age:15,
+gender: "female",
+email: "fortune445.com",
+score: 250
+}
+console.log(studentTwo)
+
+
+
+const studentThree={
+firstName:"fortune",
+lastName: "gilbert",
+phoneNo: "0812665431",
+address: "choba",
+age:15,
+gender: "male",
+email: "fortune445.com",
+score: 250
+}
+console.log(studentThree)
+
+
+
+const studentFour={
+firstName:"fortune",
+lastName: "gilberte",
+phoneNo: "0812665431",
+address: "choba",
+age:15,
+gender: "male",
+email: "fortune445.com",
+score: 250
+}
+console.log(studentFour)
+
+
+
+const studentFive={
+firstName:"fortune",
+lastName: "gilbert",
+phoneNo: "0812665431",
+address: "choba",
+age:15,
+gender: "male",
+email: "fortune445.com",
+score: 250
+}
+console.log(studentFive)
+
+
+
+
+
+
+
+
+
+
